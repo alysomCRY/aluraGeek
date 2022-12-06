36c8165783ef4766be7c72ebbfe91696a32342bb
alysom crystyam <alysom.3d@live.com>
2022-12-06 21:30:42 +0000

    alura geek

---
+++ alura.js
@@ -0,0 +1,13 @@
+const mudarImg = document.getElementById("trocarImg");
+
+const verProduto = document.getElementById("verProduto");
+
+verProduto.addEventListener("click",trocarImg) 
+ 
+
+function trocarImg () {
+    mudarImg.src = "imagens/img 02/diversos1.png"
+}
+
+
+
