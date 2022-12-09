130e2e85cae22f92e983f1cf2b0155c5e2a23f0f
alysom crystyam <alysom.3d@live.com>
2022-12-09 21:48:44 +0000

    alurageek

--- alura.js
+++ alura.js
@@ -1,13 +1,18 @@
-const mudarImg = document.getElementById("trocarImg");
-
-const verProduto = document.getElementById("verProduto");
-
-verProduto.addEventListener("click",trocarImg) 
- 
-
-function trocarImg () {
-    mudarImg.src = "imagens/img 02/diversos1.png"
+function img01() {
+    document.getElementById("trocarImg").src = "/imagens/img02/diversos1.png";
 }
-
-
-
+function img02() {
+    document.getElementById("trocarImg").src = "/imagens/img02/banner-image.png";
+}
+function img03() {
+    document.getElementById("trocarImg").src = "/imagens/img02/diversos3.png";
+}
+function img04() {
+    document.getElementById("trocarImg").src = "/imagens/img02/diversos4.png";
+}
+function img05() {
+    document.getElementById("trocarImg").src = "/imagens/img02/diversos5.png";
+}
+function img06() {
+    document.getElementById("trocarImg").src = "/imagens/img02/console6.png";
+}
