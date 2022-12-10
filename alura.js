233b9fcd340cc55591ae63c83156e1ae7ff03cb0
alysom crystyam <alysom.3d@live.com>
2022-12-10 12:46:38 +0000

    alurageek

--- alura.js
+++ alura.js
@@ -1,18 +1,7 @@
-function img01() {
-    document.getElementById("trocarImg").src = "/imagens/img02/diversos1.png";
-}
-function img02() {
-    document.getElementById("trocarImg").src = "/imagens/img02/banner-image.png";
-}
-function img03() {
-    document.getElementById("trocarImg").src = "/imagens/img02/diversos3.png";
-}
-function img04() {
-    document.getElementById("trocarImg").src = "/imagens/img02/diversos4.png";
-}
-function img05() {
-    document.getElementById("trocarImg").src = "/imagens/img02/diversos5.png";
-}
-function img06() {
-    document.getElementById("trocarImg").src = "/imagens/img02/console6.png";
-}
+function alterarImagem(imagem) {
+    // Obter a imagem atualmente exibida
+    var imagemElemento = document.getElementById('imagem');
+
+    // Alterar o atributo "src" da imagem para a nova imagem
+    imagemElemento.src = imagem;
+ }
